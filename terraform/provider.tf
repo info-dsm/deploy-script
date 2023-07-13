419bcfd347e694755d551d30ffde5da856b33877
jinu <jinwoo794533@gmail.com>
2023-07-14 00:14:33 +0900

    remove variables

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -26,9 +26,7 @@ provider "cloudflare" {
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  region = var.aws_region
 }
 
 provider "random" {
