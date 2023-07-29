92040c1782629b2cbb21e8cb1fe770fcdb5bbcb0
jinu <jinwoo794533@gmail.com>
2023-07-29 23:59:22 +0900

    d

--- terraform/aws-config.tf
+++ terraform/aws-config.tf
@@ -105,13 +105,18 @@ resource "aws_key_pair" "local-key" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["ubuntu"] //ubuntu
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
 
